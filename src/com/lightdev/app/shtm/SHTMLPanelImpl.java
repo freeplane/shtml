ab941c88df90152faebe3dc9c05f9151e2186342
dpolivaev <dpolivaev>
2007-07-07 21:01:49 +0000

    no message

--- src/com/lightdev/app/shtm/SHTMLPanelImpl.java
+++ src/com/lightdev/app/shtm/SHTMLPanelImpl.java
@@ -256,7 +256,7 @@ class SHTMLPanelImpl extends SHTMLPanel implements CaretListener{
           return true;
       menuBar.handleKeyBinding(ks, e, condition, pressed);
       // if ctrl or shift or alt halted assume that the event has been handled
-      return e.getModifiersEx() != 0;
+      return e.getModifiersEx() != 0 || e.getKeyChar() == KeyEvent.CHAR_UNDEFINED;
   }
 
 /**
