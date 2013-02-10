4d5f9bd7e529b59e898d885f44cf155c45e14017
dpolivaev <dpolivaev>
2013-02-10 09:46:40 +0100

    version 0.16.07

--- src/com/lightdev/app/shtm/FrmMain.java
+++ src/com/lightdev/app/shtm/FrmMain.java
@@ -51,7 +51,7 @@ import javax.swing.JFrame;
  */
 class FrmMain extends JFrame {
     public static final String APP_NAME = "SimplyHTML";
-    public static final String VERSION = "0.16.06";
+    public static final String VERSION = "0.16.07";
     /** static reference to this instance of class FrmMain */
     private SHTMLPanelImpl mainPane;
 
