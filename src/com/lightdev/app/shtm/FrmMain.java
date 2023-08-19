4854821e9cb91723ca656b212b81840e1838f108
dpoli <dpoli@DESKTOP-BE970F2>
2023-08-19 12:01:57 +0200

    version 0.19.7

--- src/com/lightdev/app/shtm/FrmMain.java
+++ src/com/lightdev/app/shtm/FrmMain.java
@@ -51,7 +51,7 @@ import javax.swing.JFrame;
  */
 class FrmMain extends JFrame {
     public static final String APP_NAME = "SimplyHTML";
-    public static final String VERSION = "0.19.6";
+    public static final String VERSION = "0.19.7";
     /** static reference to this instance of class FrmMain */
     private SHTMLPanelImpl mainPane;
 
